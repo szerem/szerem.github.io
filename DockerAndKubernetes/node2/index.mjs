c7bf7deffe44bc256e58d3ae1b9e52888f9869ac
szerem <szerem@go2.pl>
2025-01-02 00:08:39 +0100

    docker node and redis

---
+++ DockerAndKubernetes/node2/index.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import { createClient } from "redis";
+
+const PORT = 8080;
+
+const client = createClient({ url: "redis://redis-svc:6379" });
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+await client.connect();
+console.log(await client.ping()); // 'PONG'
+await client.set("visits", 0);
+
+/// web
+const app = express();
+
+app.get("/visits", async (req, res) => {
+  const visits = parseInt(await client.get("visits"));
+  res.send(`Number of visits is ${visits}`);
+  await client.set("visits", visits+1);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Hello world`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+// await subscriber.disconnect();
--- DockerAndKubernetes/node2/readme.md
+++ DockerAndKubernetes/node2/readme.md
@@ -5,3 +5,10 @@ docker build --no-cache --progress=plain -t my-node .
 docker run -p 5100:8080 my-node 
 docker exec <id> ps 
 
+
+docker build -t my-node .
+docker run -p 5100:8080 my-node 
+docker exec -it <id> sh 
+
+
+docker-compose up --build 
